4e6e30f76adf0b379e9006f115051751bed86656
kaname87 <kaname.nakanishi@bibo.com.ph>
2019-01-12 01:26:29 +0800

    refactor

--- similar_word_finder.py
+++ similar_word_finder.py
@@ -2,11 +2,14 @@ import csv
 from gensim.models import KeyedVectors
 from const import *
 
+def load_model():
+    model_dir = './word2vec/entity_vector/entity_vector.model.bin'
+    return KeyedVectors.load_word2vec_format(model_dir, binary=True)
+
 def find_and_write_similar_words(target_words, topn):
     csv_header = ['target_word', 'similar_word', 'similarity']
 
-    model_dir = './word2vec/entity_vector/entity_vector.model.bin'
-    model = KeyedVectors.load_word2vec_format(model_dir, binary=True)
+    model = load_model()
     with open(WORD_SOURCE_FILE_NAME, 'w') as f:
         writer = csv.writer(f, lineterminator='\n') 
         writer.writerow(csv_header)
@@ -16,6 +19,13 @@ def find_and_write_similar_words(target_words, topn):
                 output = [word] + list(result) 
                 writer.writerow(output)
 
-# if __name__ == "__main__":
+def find_similar_word(word, topn=10):
+    model = load_model()
+    return model.most_similar(f'{word}', topn=topn)
+
+if __name__ == "__main__":
+    # word = '檸檬'
+    # print(find_similar_word(word, 20))
+
 #     target_words = ['米', '津', '玄', '師']
 #     find_and_write_similar_words(target_words, 15)
